c9bdbcace1e5076cd77b093f4dbc895483284fac
Nya-moe <moe-hacker@outlook.com>
2023-05-31 02:17:29 -0400

    字体居中

--- logo.py
+++ logo.py
@@ -85,12 +85,10 @@ else:
     pyplot.ylim(-6.3, 6.3)
 # Draw the title.
 if no_text==False:
-    pyplot.title("r u r i", color='#fee4d0', y=-0.05, x=0.49, fontsize=39, fontweight="ultralight")
-    pyplot.text(-0.1, -0.12, "Linux containers, simple & secure", fontsize=16,fontweight="ultralight", transform=axes.transAxes, color='#fee4d0')
+    pyplot.title("r u r i", color='#fee4d0', y=-0.05, x=0.475, fontsize=39, fontweight="ultralight")
+    pyplot.text(-0.12, -0.12, "Linux containers, simple & secure", fontsize=16,fontweight="ultralight", transform=axes.transAxes, color='#fee4d0')
     # Save as logo.png.
     pyplot.savefig('logo.png', transparent=True)
-    pyplot.show()
 else:
     # Save as logo.svg.
     pyplot.savefig('logo.svg', transparent=True)
-    pyplot.show()
