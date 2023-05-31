40f7cf03b5db39eac6430a450b3f21d9485e0ca9
Moe-hacker <admin@crack.moe>
2023-05-31 02:37:08 -0400

    修改logo粗细

--- logo.py
+++ logo.py
@@ -48,35 +48,35 @@ else:
     pyplot.subplots_adjust(left=0,bottom=0,right=1,top=1)
 axes.set_aspect(1)
 # Draw the sectors.
-draw_circle = patches.Wedge((0, 6), 6, 210, 330, fill=False, linewidth=0.6, color='#fee4d0')
+draw_circle = patches.Wedge((0, 6), 6, 210, 330, fill=False, linewidth=1.5, color='#fee4d0')
 axes.add_artist(draw_circle)
-draw_circle = patches.Wedge((0, -6), 6, 30, 150, fill=False, linewidth=0.6, color='#fee4d0')
+draw_circle = patches.Wedge((0, -6), 6, 30, 150, fill=False, linewidth=1.5, color='#fee4d0')
 axes.add_artist(draw_circle)
-draw_circle = patches.Wedge((math.sqrt(3) * 3, 3), 6, 150, 270, fill=False, linewidth=0.6, color='#fee4d0')
+draw_circle = patches.Wedge((math.sqrt(3) * 3, 3), 6, 150, 270, fill=False, linewidth=1.5, color='#fee4d0')
 axes.add_artist(draw_circle)
-draw_circle = patches.Wedge((math.sqrt(3) * 3 * (-1), 3), 6, 270, 390, fill=False, linewidth=0.6, color='#fee4d0')
+draw_circle = patches.Wedge((math.sqrt(3) * 3 * (-1), 3), 6, 270, 390, fill=False, linewidth=1.5, color='#fee4d0')
 axes.add_artist(draw_circle)
-draw_circle = patches.Wedge((math.sqrt(3) * 3, -3), 6, 90, 210, fill=False, linewidth=0.6, color='#fee4d0')
+draw_circle = patches.Wedge((math.sqrt(3) * 3, -3), 6, 90, 210, fill=False, linewidth=1.5, color='#fee4d0')
 axes.add_artist(draw_circle)
-draw_circle = patches.Wedge((math.sqrt(3) * 3 * (-1), -3), 6, 330, 450, fill=False, linewidth=0.6, color='#fee4d0')
+draw_circle = patches.Wedge((math.sqrt(3) * 3 * (-1), -3), 6, 330, 450, fill=False, linewidth=1.5, color='#fee4d0')
 axes.add_artist(draw_circle)
 '''
 # Draw the hexagons, unnecessary because patches.Wedge() draws it as well.
-pyplot.plot([-0.00039, -0.00039], [-6, 6], linewidth=0.6, color='#fee4d0')
-pyplot.plot([math.sqrt(3) * 3, 0], [3, 6], linewidth=0.6, color='#fee4d0')
-pyplot.plot([math.sqrt(3) * 3 * (-1), math.sqrt(3) * 3 * (-1)], [3, -3], linewidth=0.6, color='#fee4d0')
-pyplot.plot([math.sqrt(3) * 3, math.sqrt(3) * 3], [3, -3], linewidth=0.6, color='#fee4d0')
-pyplot.plot([math.sqrt(3) * 3, 0], [-3, -6], linewidth=0.6, color='#fee4d0')
-pyplot.plot([math.sqrt(3) * 3 * (-1), 0], [3, 6], linewidth=0.6, color='#fee4d0')
-pyplot.plot([math.sqrt(3) * 3 * (-1), 0], [-3, -6], linewidth=0.6, color='#fee4d0')
+pyplot.plot([-0.00039, -0.00039], [-6, 6], linewidth=1.5, color='#fee4d0')
+pyplot.plot([math.sqrt(3) * 3, 0], [3, 6], linewidth=1.5, color='#fee4d0')
+pyplot.plot([math.sqrt(3) * 3 * (-1), math.sqrt(3) * 3 * (-1)], [3, -3], linewidth=1.5, color='#fee4d0')
+pyplot.plot([math.sqrt(3) * 3, math.sqrt(3) * 3], [3, -3], linewidth=1.5, color='#fee4d0')
+pyplot.plot([math.sqrt(3) * 3, 0], [-3, -6], linewidth=1.5, color='#fee4d0')
+pyplot.plot([math.sqrt(3) * 3 * (-1), 0], [3, 6], linewidth=1.5, color='#fee4d0')
+pyplot.plot([math.sqrt(3) * 3 * (-1), 0], [-3, -6], linewidth=1.5, color='#fee4d0')
 '''
 # Draw the diagonals.
 if no_text==False:
-    pyplot.plot([-0.003, -0.003], [-6, 6], linewidth=0.6, color='#fee4d0')
+    pyplot.plot([-0.003, -0.003], [-6, 6], linewidth=1.5, color='#fee4d0')
 else:
-    pyplot.plot([0, 0], [-6, 6], linewidth=0.6, color='#fee4d0')
-pyplot.plot([math.sqrt(3) * 3 * (-1), math.sqrt(3) * 3], [-3, 3], linewidth=0.6, color='#fee4d0')
-pyplot.plot([math.sqrt(3) * 3, math.sqrt(3) * 3 * (-1)], [-3, 3], linewidth=0.6, color='#fee4d0')
+    pyplot.plot([0, 0], [-6, 6], linewidth=1.5, color='#fee4d0')
+pyplot.plot([math.sqrt(3) * 3 * (-1), math.sqrt(3) * 3], [-3, 3], linewidth=1.5, color='#fee4d0')
+pyplot.plot([math.sqrt(3) * 3, math.sqrt(3) * 3 * (-1)], [-3, 3], linewidth=1.5, color='#fee4d0')
 if no_text==False:
     pyplot.xlim(-8, 8)
     pyplot.ylim(-8, 8)
@@ -85,8 +85,8 @@ else:
     pyplot.ylim(-6.3, 6.3)
 # Draw the title.
 if no_text==False:
-    pyplot.title("r u r i", color='#fee4d0', y=-0.05, x=0.475, fontsize=39, fontweight="ultralight")
-    pyplot.text(-0.12, -0.12, "Linux containers, simple & secure", fontsize=16,fontweight="ultralight", transform=axes.transAxes, color='#fee4d0')
+    pyplot.title("r u r i", color='#fee4d0', y=-0.05, x=0.475, fontsize=39, fontweight="light")
+    pyplot.text(-0.12, -0.12, "Linux containers, simple & secure", fontsize=16,fontweight="light", transform=axes.transAxes, color='#fee4d0')
     # Save as logo.png.
     pyplot.savefig('logo.png', transparent=True)
 else:
