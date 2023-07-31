9554a4d07b620524c25fb9ec2faa126992dbb6d0
Moe-hacker <admin@crack.moe>
2023-07-31 16:47:54 -0400

    fix bugs

--- ruri.h
+++ ruri.h
@@ -152,7 +152,7 @@ void show_examples();
 int mkdirs(char *dir, mode_t mode);
 void add_to_list(cap_value_t *list, int length, cap_value_t cap);
 void del_from_list(cap_value_t *list, int length, cap_value_t cap);
-struct CONTAINERS *add_node(char *container_dir, char *unshare_pid, char *drop_caplist[CAP_LAST_CAP + 1], char *env[MAX_ENVS], char *mountpoint[MAX_MOUNTPOINTS], struct CONTAINERS *container);
+struct CONTAINERS *add_node(char *container_dir, char *unshare_pid, char drop_caplist[CAP_LAST_CAP + 1][128], char *env[MAX_ENVS], char mountpoint[MAX_MOUNTPOINTS][PATH_MAX], struct CONTAINERS *container);
 struct CONTAINERS *read_node(char *container_dir, struct CONTAINERS *container);
 struct CONTAINERS *del_container(char *container_dir, struct CONTAINERS *container);
 bool container_active(char *container_dir, struct CONTAINERS *container);
