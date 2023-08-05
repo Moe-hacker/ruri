90988d01c026a8405d22efbc1da6d9ecebac38c0
Moe-hacker <admin@crack.moe>
2023-08-05 06:20:50 -0400

    删除：     shell/*

--- shell/capset.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-DROP_CAPLIST=$(whiptail --title "Capability list" --checklist \
-  "Choose which capability should be dropped\nUse space key to select, press Enter to continue" 16 60 8 \
-  ${CAPLIST} 3>&1 1>&2 2>&3)
-echo ${DROP_CAPLIST}
