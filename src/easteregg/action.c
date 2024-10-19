2a12e96ce3c0ca764986c4672277ad451a5620d3
Moe-hacker <admin@crack.moe>
2024-10-19 20:02:23 +0800

    Update WIP easteregg.

--- src/easteregg/action.c
+++ src/easteregg/action.c
@@ -1,4 +1,37 @@
+// SPDX-License-Identifier: MIT
+/*
+ *
+ * This file is part of ruri, with ABSOLUTELY NO WARRANTY.
+ *
+ * MIT License
+ *
+ * Copyright (c) 2024 Moe-hacker
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ */
 #include "include/nekofeng.h"
+/*
+ * This file defines the actions of the animation.
+ * All the functions have the same arguments.
+ */
 void face(__useconds_t inr, unsigned int keep)
 {
 	struct ACTION *action = NULL;
--- src/easteregg/include/nekofeng.h
+++ src/easteregg/include/nekofeng.h
@@ -1,3 +1,32 @@
+// SPDX-License-Identifier: MIT
+/*
+ *
+ * This file is part of ruri, with ABSOLUTELY NO WARRANTY.
+ *
+ * MIT License
+ *
+ * Copyright (c) 2024 Moe-hacker
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ */
 #define _GNU_SOURCE
 #include <errno.h>
 #include <stdio.h>
@@ -11,26 +40,44 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <stdatomic.h>
+// This program should be compiled with -lpthread.
 #include <pthread.h>
+// WARNING: magic number! Do not change!
 #define X_SIZE 36
 #define Y_SIZE 2
 struct LAYER {
+	/*
+	 * This is the smallest unit of the animation.
+	 * It represents one frame of an action.
+	 * The name is taken by historical reasons.
+	 */
+	// The string to be printed.
 	char *layer;
+	// The offset of the x-axis.
 	int x_offset;
+	// The offset of the y-axis.
 	int y_offset;
 };
 struct ACTION {
+	/*
+	 * This is a doubly linked list,
+	 * because we want to playback the action.
+	 */
+	// The layer to be printed.
 	struct LAYER *layer;
+	// The pointer to the last action.
 	struct ACTION *prior;
+	// The pointer to the next action.
 	struct ACTION *next;
 };
-struct MSG {
-	char *msg;
-	struct MSG *next;
-};
+// The offset of the x-axis.
 extern int x;
+// The offset of the y-axis.
 extern int y;
+// The lock for multi-threading.
+// This lock is used for print_layer() and clear_layer().
 extern atomic_flag lock;
+// Function list.
 void spin_lock(atomic_flag *lock);
 void spin_unlock(atomic_flag *lock);
 void play_action(struct ACTION *action, __useconds_t inr, unsigned int keep);
