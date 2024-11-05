e6218bde3a54edd8f032a72fdfcfbdfcd5597687
Moe-hacker <moe-hacker@outlook.com>
2024-11-05 22:55:29 +0800

    Try to fix loong64 action

--- build/build-loong64.sh
+++ build/build-loong64.sh
@@ -3,7 +3,10 @@ git clone https://github.com/qemu/qemu.git
 cd qemu
 ./configure --static --disable-system --target-list=loongarch64-linux-user
 make -j$(nproc)
-cp build/loongarch64-linux-user/qemu-loongarch64 /usr/bin/qemu-loongarch64-static
+sudo cp build/loongarch64-linux-user/qemu-loongarch64 /usr/bin/qemu-loongarch64-static
+suco cp build/qemu-loongarch64 /usr/bin/qemu-loongarch64-static
+sudo cp qemu-loongarch64 /usr/bin/qemu-loongarch64-static
+cd ..
 sudo debootstrap --foreign --arch=loong64 --variant=buildd --include=debian-ports-archive-keyring --verbose --components=main --resolve-deps --extra-suites=unreleased unstable ./debian http://ftp.ports.debian.org/debian-ports
 sudo cp /usr/bin/qemu-loongarch64-static debian/usr/bin/
 sudo chroot ./debian /debootstrap/debootstrap --second-stage
