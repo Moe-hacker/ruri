e8490b638100b924911e31defe2f8ed85dd0260b
Moe-hacker <moe-hacker@outlook.com>
2024-11-05 23:21:56 +0800

    Try to fix loongarch64 build

--- build/build-loong64.sh
+++ build/build-loong64.sh
@@ -9,7 +9,6 @@ sudo cp qemu-loongarch64 /usr/bin/qemu-loongarch64-static
 cd ..
 sudo debootstrap --foreign --arch=loong64 --variant=buildd --include=debian-ports-archive-keyring --verbose --components=main --resolve-deps --extra-suites=unreleased unstable ./debian http://ftp.ports.debian.org/debian-ports
 sudo cp /usr/bin/qemu-loongarch64-static debian/usr/bin/
-sudo chroot ./debian /debootstrap/debootstrap --second-stage
 cat <<EOF >> ./debian/build.sh
 rm /etc/resolv.conf
 echo nameserver 1.1.1.1 > /etc/resolv.conf
@@ -22,5 +21,6 @@ make
 tar -cvf ../loong64.tar ./ruri ./LICENSE
 EOF
 sudo chmod +x ./debian/build.sh
-sudo ./ruri/ruri -a loong64 -q qemu-loongarch64-static ./debian /bin/sh /build.sh
+./ruri/ruri -a loong64 -q /usr/bin/qemu-loongarch64-static ./debian /debootstrap/debootstrap --second-stage
+sudo ./ruri/ruri -a loong64 -q /usr/bin/qemu-loongarch64-static ./debian /bin/sh /build.sh
 cp ./debian/loong64.tar ../loongarch64.tar
