9eae820bc2d5ffc36d3db7e792646eda7cf1b27f
Moe-hacker <admin@crack.moe>
2024-11-08 18:48:04 +0800

    Trigger test

--- test/global.sh
+++ test/global.sh
@@ -6,6 +6,7 @@ export BLUE="\033[34m"
 export PURPLE="\033[35m"
 export CYAN="\033[36m"
 export CLEAR="\033[0m"
+
 function error() {
   cd ${TEST_ROOT}
   source clean.sh
@@ -17,6 +18,7 @@ function error() {
   printf "${RED}$@\n${CLEAR}"
   exit 1
 }
+
 function check_if_succeed() {
   if [[ $1 == "0" ]]; then
     return 0
@@ -26,6 +28,7 @@ function check_if_succeed() {
     error "failed!"
   fi
 }
+
 function check_if_failed() {
   if [[ $1 == "0" ]]; then
     error "succeed, but expected failed!"
@@ -33,22 +36,26 @@ function check_if_failed() {
     return 0
   fi
 }
+
 function show_test_description() {
   echo -e "${BASE}\nRunning test ${GREEN}#${TEST_NO}"
   echo -e "${BASE}Description:\n${CYAN} ${DESCRIPTION}"
   printf "${CLEAR}\n"
   sleep 2
 }
+
 function show_subtest_description() {
   echo -e "${BASE}\nRunning subtest ${GREEN}#${SUBTEST_NO} ${BASE}of test ${GREEN}#${TEST_NO}"
   echo -e "${BASE}Subtest Description:\n${CYAN} ${SUBTEST_DESCRIPTION}"
   printf "${CLEAR}\n"
   sleep 2
 }
+
 function pass_test() {
   echo -e "${BASE}\nPassed test${GREEN} #${TEST_NO}"
   printf "${CLEAR}\n"
 }
+
 function pass_subtest() {
   echo -e "${BASE}\nPassed subtest${GREEN} #${SUBTEST_NO} ${BASE}of test ${GREEN}#${TEST_NO}"
   printf "${CLEAR}\n"
