65fb5c636c2720674b680982e0538b4b35720ec5
Moe-hacker <moe-hacker@outlook.com>
2024-11-21 04:02:48 +0000

    move real main() into ruri().

--- src/include/ruri.h
+++ src/include/ruri.h
@@ -247,6 +247,7 @@ gid_t get_user_gid(const char *_Nonnull username);
 pid_t get_ns_pid(const char *_Nonnull container_dir);
 void ruri_fetch(void);
 void correct_config(const char *_Nonnull path);
+int ruri(int argc, char **argv);
 //   ██╗ ██╗  ███████╗   ████╗   ███████╗
 //  ████████╗ ██╔════╝ ██╔═══██╗ ██╔════╝
 //  ╚██╔═██╔╝ █████╗   ██║   ██║ █████╗
